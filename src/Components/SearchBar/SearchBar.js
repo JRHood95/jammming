66148eea2e11f05b8f276ae7b7060fe21ab626b7
JRHood95 <hoodjulian@yahoo.co.uk>
2018-06-03 01:36:36 +0700

    Enter key now triggers search

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -11,6 +11,7 @@ class SearchBar extends React.Component {
 
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
+        this.enterPressed = this.enterPressed.bind(this)
     }
 
     // SET THE ARGUMENT OF THE SEARCH METHOD TO THE STATE OF TERM
@@ -23,10 +24,21 @@ class SearchBar extends React.Component {
         this.setState({term: event.target.value})
     }
 
+    // TRIGGER SEARCH ONCE 'ENTER' KEY IS PRESSED
+    enterPressed(event) {
+        let code = event.keyCode || event.which; // Use either keyCode or which, depending on browser support
+        if(code === 13) { // 13 is the 'enter' key code
+            this.search();
+        }
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
+                <input placeholder="Enter A Song, Album, or Artist"
+                       onChange={this.handleTermChange}
+                       onKeyPress={this.enterPressed}
+                />
                 <a onClick={this.search}>SEARCH</a>
             </div>
         );
